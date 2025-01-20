b294d710c5ea233498dfdcd37b871ce56ec7edaf
jalynanicole <jalyna.neudorf@ucalgary.ca>
2025-01-19 23:40:12 -0700

    info

--- src/data/data.js
+++ src/data/data.js
@@ -1,8 +1,8 @@
 export const data = {
   name: "Jalyna Neudorf",
-  bio: "Undergraduate Architectural Student at University of Calgary passionate about building tools to make homes more affordable.",
+  bio: "Undergraduate Architectural Student at University of Calgary.",
   contact: {
-    email: "john.doe@gmail.com",
+    email: "jalyna.neudorf@ucalgary.ca",
     phone: "+12345678910",
     linkedin: "https://www.linkedin.com/",
     github: "https://www.github.com/",
